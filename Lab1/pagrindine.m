28d9b8b1c75dce481eb9f6254a4ca829dac7980e
salvi195 <rasiukeviciute.salvija@gmail.com>
2026-09-09 15:53:52 +0300

    Add files via upload

---
+++ Lab1/pagrindine.m
@@ -0,0 +1,20 @@
+x = 1:32;
+y = x.^2;
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+ylabel('F_1 [-o-]  |   F_2  [-x-]')
+
+N = 6;
+v = (N+1):0.5:(N+4);
+
+A =  [N,   N+1, N+2;
+      N+3, N+4, N+5;
+      N+6, N+7, N+8 ]
+
+a = A(3, 2);
+b = A(2:3, 1:2);
+c = A([1, 3], [1, 3]);
+
+v3 = v([2, 4, 6]);
+newA = round([A; v3])
